7376f551deb62840dcf1e43247fc2005b026f38e
YTae_Jun <dbsxowns34@gmail.com>
2020-10-13 13:52:53 +0900

    [infoPet_2] test 2020 10 13

---
+++ 2.InfoPet/infoPet_2/src/main/java/info/infoPet/InfoPet2Application.java
@@ -0,0 +1,13 @@
+package info.infoPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoPet2Application.class, args);
+	}
+
+}
